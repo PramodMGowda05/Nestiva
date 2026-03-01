585ec4a370b04127a6639bf2c88f6d3c6c551b87
PramodMGowda05 <pramodgowda3311@gmail.com>
2026-03-01 23:48:10 +0530

    Server-Side error Handling

--- app.js
+++ app.js
@@ -4,14 +4,16 @@ const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
-const ejsMate = require('ejs-mate'); 
+const ejsMate = require('ejs-mate');
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js")
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine('ejs', ejsMate);
-app.use(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 const port = 8080;
 const MONGO_URL = "mongodb://127.0.0.1:27017/nestiva";
@@ -49,11 +51,15 @@ app.get("/listings/:id", async (req, res) => {
 });
 
 //Create route
-app.post("/listings", async (req, res) => {
+app.post("/listings", wrapAsync(async (req, res, next) => {
+    if (!req.body.listing) {
+        throw new ExpressError(400, "Send Valid data for listing");
+    }
     const newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
-});
+})
+);
 
 //Edit route
 app.get("/listings/:id/edit", async (req, res) => {
@@ -90,6 +96,16 @@ app.delete("/listings/:id", async (req, res) => {
 //    console.log("sample was saved");
 //    res.send("Successfully tested");
 // });
+
+app.all("*splat", (req, res, next) => {
+    next(new ExpressError(404, "Page Not Found!"));
+});
+
+app.use((err, req, res, next) => {
+    let { statusCode = 500, message = "Something went wrong!" } = err;
+    res.status(statusCode).render("error.ejs");
+    //res.status(statusCode).send(message);
+});
 
 app.listen(port, () => {
     console.log("The app is running on port:", port);

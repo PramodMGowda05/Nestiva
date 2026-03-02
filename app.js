1fdd1291ff398bf91748317586ae05541acc9759
PramodMGowda05 <pramodgowda3311@gmail.com>
2026-03-02 23:23:19 +0530

    Validation

--- app.js
+++ app.js
@@ -4,14 +4,17 @@ const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
-const ejsMate = require('ejs-mate'); 
+const ejsMate = require('ejs-mate');
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js")
+const { listingSchema } = require("./schema.js");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine('ejs', ejsMate);
-app.use(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 const port = 8080;
 const MONGO_URL = "mongodb://127.0.0.1:27017/nestiva";
@@ -29,6 +32,17 @@ async function main() {
 app.get("/", (req, res) => {
     res.send("Hi, I am root");
 });
+
+const validateListing = (req, res, next) => {
+    let { error } = listingSchema.validate(req.body);
+    if (error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errMsg    
+        );
+    } else {
+        next();
+    }
+}
 
 //Index route
 app.get("/listings", async (req, res) => {
@@ -49,11 +63,14 @@ app.get("/listings/:id", async (req, res) => {
 });
 
 //Create route
-app.post("/listings", async (req, res) => {
-    const newListing = new Listing(req.body.listing);
-    await newListing.save();
-    res.redirect("/listings");
-});
+app.post("/listings",
+    validateListing,
+    wrapAsync(async (req, res, next) => {
+        const newListing = new Listing(req.body.listing);
+        await newListing.save();
+        res.redirect("/listings");
+    })
+);
 
 //Edit route
 app.get("/listings/:id/edit", async (req, res) => {
@@ -63,11 +80,15 @@ app.get("/listings/:id/edit", async (req, res) => {
 });
 
 //Update ruute
-app.put("/listings/:id", async (req, res) => {
-    let { id } = req.params;
-    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
-    res.redirect(`/listings/${id}`);
-});
+app.put("/listings/:id",
+    validateListing,
+    wrapAsync(
+        async (req, res) => {
+            let { id } = req.params;
+            await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+            res.redirect(`/listings/${id}`);
+        })
+);
 
 //Delete route
 app.delete("/listings/:id", async (req, res) => {
@@ -90,6 +111,16 @@ app.delete("/listings/:id", async (req, res) => {
 //    console.log("sample was saved");
 //    res.send("Successfully tested");
 // });
+
+app.all("*splat", (req, res, next) => {
+    next(new ExpressError(404, "Page Not Found!"));
+});
+
+app.use((err, req, res, next) => {
+    let { statusCode = 500, message = "Something went wrong!" } = err;
+    res.status(statusCode).render("error.ejs");
+    //res.status(statusCode).send(message);
+});
 
 app.listen(port, () => {
     console.log("The app is running on port:", port);

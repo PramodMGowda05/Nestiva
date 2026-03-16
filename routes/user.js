bba33f41c726a31a36c616c45a26ef9ce7eb9e85
PramodMGowda05 <pramodgowda3311@gmail.com>
2026-03-16 22:54:51 +0530

    Styling - login after sign up & post login

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync.js");
 const passport = require("passport");
+const { saveRedirectUrl } = require("../middleware.js")
 
 router.get("/signup", (req, res) => {
     res.render("users/signup.ejs")
@@ -14,8 +15,14 @@ router.post("/signup", wrapAsync(async (req, res) => {
         const newUser = new User({ email, username });
         const regiseterdUser = await User.register(newUser, password);
         console.log(regiseterdUser);
-        req.flash("success", "Welcome to Nestiva!");
-        res.redirect("/listings");
+        req.login(regiseterdUser, (err) => {
+            if (err) {
+                return next(err);
+            }
+            req.flash("success", "Welcome to Nestiva!");
+            res.redirect("/listings");
+        })
+
     } catch (e) {
         req.flash("error", e.message);
         res.redirect("/signup");
@@ -30,20 +37,22 @@ router.get("/login", (req, res) => {
 });
 
 router.post("/login",
+    saveRedirectUrl,
     passport.authenticate('local', {
         failureRedirect: '/login',
         failureFlash: true,
     }),
     async (req, res) => {
-        req.flash("success","Welocome back to Nestiva!");
-        res.redirect("/listings");
+        req.flash("success", "Welocome back to Nestiva!");
+        let redirectUrl = res.locals.redirectUrl || "/listings";
+        res.redirect(redirectUrl);
     }
 );
 
-router.get("/logout", (req , res ) => {
+router.get("/logout", (req, res) => {
     req.logout((err) => {
-        if(err) {
-           return next(err);
+        if (err) {
+            return next(err);
         }
         req.flash("success", "You are logged out!");
         res.redirect("/listings");

f1f984f74955534529af82a146b84abd0085a8fc
PramodMGowda05 <pramodgowda3311@gmail.com>
2026-03-26 23:30:23 +0530

    Parsing the image file from forms using multer

--- routes/listing.js
+++ routes/listing.js
@@ -3,17 +3,21 @@ const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
 const Listing = require("../models/listing.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
-
 const listingController = require("../controllers/listings.js");
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 router
     .route("/")
-    .get( wrapAsync(listingController.index))
-    .post(
-    isLoggedIn,
-    validateListing,
-    wrapAsync(listingController.createListing)
-);
+    .get(wrapAsync(listingController.index))
+    //     .post(
+    //     isLoggedIn,
+    //     validateListing,
+    //     wrapAsync(listingController.createListing)
+    // );
+    .post(upload.single('listing[image]'), (req, res) => {
+        res.send(req.file);
+    });
 
 //New  route
 router.get("/new", isLoggedIn, listingController.renderNewForm);
@@ -21,18 +25,18 @@ router.get("/new", isLoggedIn, listingController.renderNewForm);
 
 router
     .route("/:id")
-    .get( wrapAsync(listingController.showListing))
+    .get(wrapAsync(listingController.showListing))
     .put(
-    isLoggedIn,
-    isOwner,
-    validateListing,
-    wrapAsync(listingController.updateListing)
+        isLoggedIn,
+        isOwner,
+        validateListing,
+        wrapAsync(listingController.updateListing)
     )
     .delete(
-    isLoggedIn,
-    isOwner,
-    wrapAsync(listingController.destroyListing)
-);
+        isLoggedIn,
+        isOwner,
+        wrapAsync(listingController.destroyListing)
+    );
 
 
 

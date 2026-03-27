6f4ff23edf4d97d4f2d85935add2a15093c995cf
PramodMGowda05 <pramodgowda3311@gmail.com>
2026-03-27 19:58:28 +0530

    Editing the listing image

--- controllers/listings.js
+++ controllers/listings.js
@@ -32,7 +32,7 @@ module.exports.createListing = async (req, res, next) => {
     let filename = req.file.filename;
     const newListing = new Listing(req.body.listing);
     newListing.owner = req.user._id;
-    newListing.image = { url, filename};
+    newListing.image = { url, filename };
     await newListing.save();
     req.flash("success", "New Listing Created!");
     res.redirect("/listings");
@@ -50,7 +50,17 @@ module.exports.renderEditForm = async (req, res) => {
 
 module.exports.updateListing = async (req, res) => {
     let { id } = req.params;
-    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    let listing = await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+
+    if (typeof req.file != "undefined") {
+        let url = req.file.path;
+        let filename = req.file.filename;
+        listing.image = { url, filename };
+        await listing.save();
+    }
+    let url = req.file.path;
+    let filename = req.file.filename;
+    listing.image = { url, filename };
     req.flash("success", "Listing Updated!");
     res.redirect(`/listings/${id}`);
 };

f6e3b8798e430affc1a2344e38cf2eb84567be16
PramodMGowda05 <pramodgowda3311@gmail.com>
2026-04-03 23:29:20 +0530

    fix: Page not found error for *

--- app.js
+++ app.js
@@ -111,7 +111,7 @@ app.use("/listings/:id/reviews",reviewRouter);
 app.use("/", userRouter);
 
 
-app.all("*", (req, res, next) => {
+app.all("(.*)", (req, res, next) => {
     next(new ExpressError(404, "Page Not Found!"));
 });
 
